04618210a75ba53357b69e5033a38078f699bc18
andres97n <novilloa21@gmail.com>
2022-05-05 18:47:03 -0500

    CONON - Update Base file

--- conon/settings/local.py
+++ conon/settings/local.py
@@ -38,11 +38,13 @@ DATABASES = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://conon-app-test.herokuapp.com"
+    "https://conon.netlify.app"
 ]
 
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
     "https://conon-app-test.herokuapp.com"
+    "https://conon.netlify.app"
 ]
 
 # Static files (CSS, JavaScript, Images)
